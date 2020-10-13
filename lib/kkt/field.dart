28a7a60025af55c3c312dbb987ebb6df1ea97d16
Pavel Zarudnev <rockinpaulz@gmail.com>
2020-10-13 17:48:06 +0300

    Field constructor updated.

--- lib/kkt/field.dart
+++ lib/kkt/field.dart
@@ -13,4 +13,28 @@ class Field {
 
   Uint8List buffer;
 
+  Field(Tag tag, int length) {
+    if (length < 0 || tag.length > 0 && length > tag.length) {
+      throw('Value\'s length must not be more than ${tag.length} bytes.');
+    } else if (tag.isFixedLength && length != tag.length) {
+      throw('Value\'s length must equal to ${tag.length} bytes.');
+    }
+    buffer = Uint8List(length + HEADER_SIZE);
+    _setTag(tag);
+    _setLength(length);
+  }
+
+  _setTag(Tag tag) {
+    _setUint16(tag.code, _TAG_OFFSET);
+  }
+
+  _setLength(int length) {
+    _setUint16(length, _LENGTH_OFFSET);
+  }
+
+  _setUint16(int value, int offset) {
+    Uint8List values = Uint8List.fromList([value, value >> 8]);
+    buffer[offset] = values[0];
+    buffer[offset + 1] = values[1];
+  }
 }
