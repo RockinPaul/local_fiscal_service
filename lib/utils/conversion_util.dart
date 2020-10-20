ecb5529e12670436cb744403853ea0eb2428622b
Pavel Zarudnev <rockinpaulz@gmail.com>
2020-10-21 02:20:01 +0300

    ConversionUtil update.

--- lib/utils/conversion_util.dart
+++ lib/utils/conversion_util.dart
@@ -6,11 +6,62 @@ import 'package:local_fiscal_service/kkt/tag.dart';
 
 class ConversionUtil {
 
-  
+  static int leToUInt16(Uint8List bytes, {int offset = 0}) {
+    return (bytes[offset + 1] & 0xFF) << 8 | (bytes[offset] & 0xFF);
+  }
 
-  Uint8List fromPrintableFiscalMark(int fiscalMark) {
+  static Uint8List uint16ToLe(Uint8List bytes, {int value, int offset = 0}) {
+    if (value != null) {
+      bytes[offset] = value & 0xFF;
+      bytes[offset + 1] = (value >> 8) & 0xFF;
+    } else {
+      bytes[offset] = 0xFF;
+      bytes[offset + 1] = 0xFF;
+    }
+    return bytes;
+  }
+
+
+  static int leToUInt32(Uint8List bytes, {int offset = 0}) {
+    return (bytes[offset + 3] & 0xFF) << 24 |
+    (bytes[offset + 2] & 0xFF) << 16 |
+    (bytes[offset + 1] & 0xFF) << 8 |
+    (bytes[offset] & 0xFF);
+  }
+
+  static Uint8List uint32ToLe(Uint8List bytes, {int value, int offset = 0}) {
+    if (value != null) {
+      bytes[offset] = value & 0xFF;
+      bytes[offset + 1] = (value >> 8) & 0xFF;
+      bytes[offset + 2] = (value >> 16) & 0xFF;
+      bytes[offset + 3] = (value >> 24) & 0xFF;
+    } else {
+      bytes[offset] = 0xFF;
+      bytes[offset + 1] = 0xFF;
+      bytes[offset + 2] = 0xFF;
+      bytes[offset + 3] = 0xFF;
+    }
+    return bytes;
+  }
+
+  static int toPrintableFiscalMark(Uint8List fiscalMark) {
+    if (fiscalMark == null) {
+      return null;
+    }
+    if (fiscalMark.length != Tag.DOCUMENT_FISCAL_MARK.length) {
+      throw Exception("Incorrect fiscal mark size");
+    }
+    int result = 0;
+    for (int i = 0; i < fiscalMark.length; i++) {
+      result <<= 8;
+      result |= (fiscalMark[i] & 0xFF);
+    }
+    return result;
+  }
+
+  static Uint8List fromPrintableFiscalMark(int fiscalMark) {
     Uint8List result = Uint8List(Tag.DOCUMENT_FISCAL_MARK.length);
-    for(int i = result.length-1; i>=0; i--) {
+    for (int i = result.length - 1; i >= 0; i--) {
       result[i] = fiscalMark & 0xFF;
       fiscalMark >>= 8;
     }
@@ -18,7 +69,7 @@ class ConversionUtil {
   }
 
   static Uint8List invertByteArray(Uint8List b) {
-    for(int i=0; i < b.length; i++){
+    for (int i = 0; i < b.length; i++) {
       b[i] = ~b[i];
     }
     return b;
@@ -35,9 +86,9 @@ class ConversionUtil {
 
   static int beToUint32(Uint8List bytes) {
     return ((bytes[0] & 255) << 24 |
-        (bytes[1] & 255) << 16 |
-        (bytes[2] & 255) << 8 |
-        bytes[3] & 255);
+    (bytes[1] & 255) << 16 |
+    (bytes[2] & 255) << 8 |
+    bytes[3] & 255);
   }
 
   static Characters hexArray = '0123456789ABCDEF'.characters;
