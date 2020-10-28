6919147663b00174de00dc3d1003213f0c6e4987
Pavel Zarudnev <rockinpaulz@gmail.com>
2020-10-28 16:56:06 +0300

    Working on network manager.

---
+++ lib/network/network_manager.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:local_fiscal_service/kkt/fn_record.dart';
+import 'package:local_fiscal_service/utils/CRC16CCITT.dart';
+
+class NetworkManager {
+  Socket _socket;
+  String _remoteIp;
+  int _remotePort;
+  int _soTimeout;
+
+  // Socket state
+  bool _socketIsClosed = true;
+
+  NetworkManager(String ip, int port, int timeout) {
+    _remoteIp = ip;
+    _remotePort = port;
+    _soTimeout = timeout;
+  }
+
+  connect(String ip, int port) {
+    _remoteIp = ip;
+    _remotePort = port;
+    reconnect();
+  }
+
+  connect() {
+    if(_socket == null || _socketIsClosed) {
+      reconnect();
+    }
+  }
+
+}
