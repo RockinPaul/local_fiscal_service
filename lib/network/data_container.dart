00c40eb35ccc44ef95d6befcd617c9ef5522a345
Pavel Zarudnev <rockinpaulz@gmail.com>
2020-10-29 23:24:07 +0300

    data_container update.

--- lib/network/data_container.dart
+++ lib/network/data_container.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:typed_data';
 
 import 'package:local_fiscal_service/kkt/fn_record.dart';
@@ -19,26 +20,91 @@ class DataContainer {
 
   static const int VERSION = 0x01;
 
-  int crc;
-  ContainerType containerType;
-  FnRecordType documentType;
-  int containerVersion;
-  ServiceData serviceData;
-  Uint8List body;
+  int _crc;
+  ContainerType _containerType;
+  FnRecordType _documentType;
+  int _containerVersion;
+  ServiceData _serviceData;
+  Uint8List _body;
 
   DataContainer({ContainerType type, Uint8List data, int offset = 0}) {
     if (type != null) {
-      containerType = type;
-      containerVersion = VERSION;
-      serviceData = ServiceData();
+      _containerType = type;
+      _containerVersion = VERSION;
+      _serviceData = ServiceData();
     } else {
-      if (data.length - offset < getHeaderSize()) {
+      if (data.length - offset < headerSize) {
         throw Exception('Data buffer if too small');
       }
       int bodySize = ConversionUtil.leToUInt16(
         data,
         offset: offset + BODY_LENGTH_OFFSET,
       );
+      if (bodySize != data.length - offset - headerSize) {
+        throw Exception('Invalid message size');
+      }
+      _crc = ConversionUtil.leToUInt16(data, offset: offset + CRC_OFFSET);
+      _containerType =
+          ContainerType.byCode(data[offset + CONTAINER_TYPE_OFFSET]);
+      _documentType = FnRecordType.byCode(data[offset + DOCUMENT_TYPE_OFFSET]);
+      _containerVersion = data[offset + CONTAINER_VERSION_OFFSET];
+      if (_containerVersion != VERSION) {
+        throw Exception('Invalid container version.');
+      }
+      _serviceData = new ServiceData(
+        data: data,
+        offset: offset + SERVICE_DATA_OFFSET,
+      );
+      List.copyRange(
+          _body,
+          0,
+          data,
+          offset + SERVICE_DATA_OFFSET + serviceData.size,
+          offset + SERVICE_DATA_OFFSET + serviceData.size + bodySize);
     }
   }
+
+  int get bodySize => _body != null ? _body.length : 0;
+
+  setBodySize(int bodySize) {
+    _body = Uint8List(bodySize);
+  }
+
+  setCrc(int crc) {
+    _crc = crc;
+  }
+
+  ContainerType get containerType => _containerType;
+
+  setContainerType(ContainerType containerType) {
+    _containerType = containerType;
+  }
+
+  FnRecordType get documentType => _documentType;
+
+  setDocumentType(FnRecordType documentType) {
+    _documentType = documentType;
+  }
+
+  Uint8List get containerVersion => this.containerVersion;
+
+  setContainerVersion(int containerVersion) {
+    _containerVersion = containerVersion;
+  }
+
+  ServiceData get serviceData => this.serviceData;
+
+  setServiceData(ServiceData serviceData) {
+    _serviceData = serviceData;
+  }
+
+  Uint8List get body => _body;
+
+  setBody(Uint8List body) {
+    _body = body;
+  }
+
+  static int get headerSize => SERVICE_DATA_OFFSET + ServiceData.getSize();
+
+  int get size => headerSize + bodySize;
 }
